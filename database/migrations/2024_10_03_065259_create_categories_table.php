eaf95962a9311622651e287bd4964db0e5dd4a88
John Emmanuel Piala <71298426+jbpiala@users.noreply.github.com>
2024-10-03 06:58:29 +0000

    Edited Files

--- database/migrations/2024_10_03_065259_create_categories_table.php
+++ database/migrations/2024_10_03_065259_create_categories_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $stable->string('name');
+            $table->string('name');
             $table->timestamps();
         });
     }
